f0ecc38d382dd5537f1dd70036532347f38dad1a
Federico Ferri <federico.ferri.it@gmail.com>
2019-02-25 10:59:28 +0100

    choose actions at random

--- myproxy.cpp
+++ myproxy.cpp
@@ -8,28 +8,41 @@ MyProxy::MyProxy(QQmlObjectListModel<Item> *model, QObject *parent)
 
 void MyProxy::test()
 {
-    static int action = 0;
+    static int itemNumber = 0;
+    static QStringList colors {"red", "green", "blue", "magenta", "orange", "gray", "navy"};
+    static QStringList tags {"Foo", "Bar", "Baz", "Woop"};
 
-    switch(action)
+    if(!itemNumber) itemNumber = model_->count();
+
+    switch(qrand() % 8)
     {
     case 0:
+    case 4:
         // add a new item
-        model_->append(new Item(QStringLiteral("Item %1").arg(model_->count() + 1), {}));
+        model_->append(new Item(QStringLiteral("Item %1").arg(++itemNumber), {}));
         break;
     case 1:
+    case 5:
+    case 6:
         // add a tag to an item
-        model_->at(model_->count() - 1)->tags()->append(new Tag("Foo", "gray"));
-        model_->at(model_->count() - 1)->tags()->append(new Tag("Bar", "navy"));
+        if(model_->isEmpty()) test();
+        else model_->at(qrand() % model_->count())->tags()->append(new Tag(tags[qrand() % tags.count()], colors[qrand() % colors.count()]));
         break;
     case 2:
+    case 7:
         // remove a tag
-        model_->at(model_->count() - 1)->tags()->remove(0);
+        if(model_->isEmpty()) test();
+        else
+        {
+            auto t = model_->at(qrand() % model_->count())->tags();
+            if(t->isEmpty()) test();
+            else t->remove(qrand() % t->count());
+        }
         break;
     case 3:
         // remove an item
-        model_->remove(model_->count() - 1);
+        if(model_->isEmpty()) test();
+        else model_->remove(qrand() % model_->count());
         break;
     }
-
-    action = (action + 1) % 4;
 }
